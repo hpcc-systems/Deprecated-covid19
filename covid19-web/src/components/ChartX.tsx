869ec61287b24cfc793dd299d6ab44bf9746b2d3
arjunachala <arjunachala@gmail.com>
2020-04-18 15:49:25 -0400

    Fixes to charts

---
+++ covid19-web/src/components/ChartX.tsx
@@ -0,0 +1,43 @@
+import React, {useEffect} from "react";
+
+interface Props {
+    readonly chart: any;
+    readonly config: object;
+    readonly data: any;
+    readonly height?: string;
+}
+
+
+export function Chart(props: Props) {
+    const [plot, setPlot] = React.useState<any>(undefined);
+    const [container, setContainer] = React.useState<HTMLElement|null>(null);
+    useEffect(() => {
+      // @ts-ignore
+        //console.log('Use effect ' + props.config.title.text + ' plot - ' + plot);
+      if (plot) {
+          plot.updateConfig(props.config);
+          plot.changeData(props.data);
+          plot.render();
+      }
+    })
+
+    useEffect(() => {
+        if(container != null) {
+            const chart = new props.chart(container, props.config);
+
+            chart.render();
+            setPlot(chart);
+        }
+
+    },[container]);
+
+
+
+    return (
+        <div style={{width:'100%', height:props.height}} ref={(e) => (setContainer(e))} />
+    )
+}
+
+Chart.defaultProps = {
+    height: '600px'
+}
