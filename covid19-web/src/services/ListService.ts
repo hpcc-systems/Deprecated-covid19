c16f72b33b1c46407afbe17bf87896251b048e70
arjunachala <arjunachala@gmail.com>
2020-05-28 12:54:22 -0400

    fixed labels

--- covid19-web/src/services/ListService.ts
+++ covid19-web/src/services/ListService.ts
@@ -16,11 +16,11 @@ export default class ListService {
                      {moduleId:'us_states', id:'us_states/states_metrics', title: 'Weekly Metrics', description: 'Weekly snapshot of metrics for US states. Please use the filter button to select the states and period.',isDefault: false},
                      {moduleId:'world_countries', id:'world_countries/countries_map', title: 'Map', description: 'Investigate trends and view models as to how each country is progressing',isDefault: true},
                      {moduleId: 'world_countries', id: 'world_countries/trends', title: 'Daily Metrics', description: `Daily snapshot of cases and deaths by countries as of ${new Date()}. Please use the filters to select the countries.`, isDefault: false},
-                     {moduleId:'world_countries', id:'world_countries/countries_metrics', title: 'Weekly Metrics', description: 'Weekly snapshot of metrics for US states. Please use the filter button to select the states and period.',isDefault: false},
+                     {moduleId:'world_countries', id:'world_countries/countries_metrics', title: 'Weekly Metrics', description: 'Weekly snapshot of metrics for US states. Please use the filter button to select the countries and period.',isDefault: false},
                      {moduleId: 'home', id: 'home/summary', title: 'Summary', description: `Summary of cases, deaths, cases increases, deaths increase as of ${new Date()}`, isDefault: true},
                      {moduleId:'us_counties', id:'us_counties/map', title: 'Map', description: 'Investigate trends and view models as to how each state is progressing',isDefault: true},
                      {moduleId:'us_counties', id:'us_counties/trends', title: 'Daily Metrics', description: `Daily snapshot of cases and deaths for US Counties as of ${new Date()}. Please use the filters to select the counties.`,  isDefault:false},
-                     {moduleId:'us_counties', id:'us_counties/counties_metrics', title: 'Weekly Metrics', description: 'Weekly snapshot of metrics for US Counties. Please use the filters to select the states and period.',isDefault: false},
+                     {moduleId:'us_counties', id:'us_counties/counties_metrics', title: 'Weekly Metrics', description: 'Weekly snapshot of metrics for US Counties. Please use the filters to select the countries and period.',isDefault: false},
 
                     ];//Inline Data
 
