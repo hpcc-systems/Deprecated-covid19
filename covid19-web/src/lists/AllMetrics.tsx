31cdecd6d6d92bad70eb7e010ba1e6a47a150167
arjunachala <arjunachala@gmail.com>
2020-06-12 16:51:04 -0400

    Removed unused code

--- covid19-web/src/lists/AllMetrics.tsx
+++ covid19-web/src/lists/AllMetrics.tsx
@@ -1,15 +1,12 @@
 import React, {useEffect, useRef, useState} from "react";
 
-import {Button, Col, Descriptions, Layout, PageHeader, Popover, Row, Table, Tabs} from "antd";
+import {Button, Descriptions, Layout, PageHeader, Table, Tabs} from "antd";
 
 import {QueryData} from "../components/QueryData";
 import {FilterRenderer} from "../components/FilterRenderer";
-import {ChartX} from "../components/ChartX";
 import Search from "antd/es/input/Search";
 import LocationDetails from "./LocationDetails";
 
-
-const {TabPane} = Tabs;
 
 interface AllMetricsProps {
     title: string;
@@ -19,9 +16,7 @@ interface AllMetricsProps {
 }
 
 export default function AllMetrics(props: AllMetricsProps) {
-    // const [queryGrouped] = React.useState<QueryData>(new QueryData('hpccsystems_covid19_query_metrics_grouped'));
-    // const [queryPeriod] = React.useState<QueryData>(new QueryData('hpccsystems_covid19_query_metrics_period'));
-    // const [queryCatalog] = React.useState<QueryData>(new QueryData('hpccsystems_covid19_query_metrics_catalog'));
+
     const queryGrouped = useRef<QueryData>(new QueryData('hpccsystems_covid19_query_metrics_grouped'));
     const queryPeriod = useRef<QueryData>(new QueryData('hpccsystems_covid19_query_metrics_period'));
     const queryCatalog = useRef<QueryData>(new QueryData('hpccsystems_covid19_query_metrics_catalog'));
@@ -29,14 +24,12 @@ export default function AllMetrics(props: AllMetricsProps) {
     const [queryPeriodFilter, setQueryPeriodFilter] = React.useState<string>('1');
 
 
-    const [locationsMeasuresData, setLocationsMeasuresData] = React.useState<any>([]);
     const [allMeasuresData, setAllMeasuresData] = React.useState<any>([]);
-    const [locationsFilter, setLocationsFilter] = React.useState<Array<string>>([]);
     const [periodFilter, setPeriodFilter] = React.useState<string>('1');
     const [periodsCatalog, setPeriodsCatalog] = React.useState<any>([]);
     const [dataLoadingStatus, setDataLoadingStatus] = React.useState<boolean>(false);
     const [tableFilterValue, setTableFilterValue] = React.useState<string>('');
-    const [filterLength, setFilterLength] = React.useState<number>(2);
+
 
     const [showLocationDetails, setShowLocationDetails] =
         useState<any>({visible: false, location: '', locationType: ''});
@@ -51,15 +44,6 @@ export default function AllMetrics(props: AllMetricsProps) {
         return a;
     }
 
-    function periodTitle(): string {
-        let title = '';
-        periodsCatalog.forEach((item: any) => {
-            if (periodFilter === item.id) {
-                title = item.title;
-            }
-        })
-        return title;
-    }
 
     useEffect(() => {
         //This will be called once. Maybe fetch the catalog info and defaults here
@@ -75,7 +59,6 @@ export default function AllMetrics(props: AllMetricsProps) {
             setAllMeasuresData(queryPeriod.current.getData('metrics_period'));
             let lf = toLocationsFilter(queryPeriod.current.getData('default_locations'));
             setQueryLocationsFilter(lf);
-            setLocationsFilter(lf);
 
         }
 
@@ -92,7 +75,6 @@ export default function AllMetrics(props: AllMetricsProps) {
             setAllMeasuresData(queryPeriod.current.getData('metrics_period'));
             let lf = toLocationsFilter(queryPeriod.current.getData('default_locations'));
             setQueryLocationsFilter(lf);
-            setLocationsFilter(lf);
         });
     }, [queryPeriodFilter, props.typeFilter]);
 
@@ -104,39 +86,27 @@ export default function AllMetrics(props: AllMetricsProps) {
         filters.set('locationsFilter', stringArrayToString(queryLocationsFilter));
         filters.set('typeFilter', props.typeFilter);
         setDataLoadingStatus(true);
-        setFilterLength(queryLocationsFilter.length);
+
         if (queryLocationsFilter.length > 0) {
             queryGrouped.current.initData(filters).then(() => {
 
-                setLocationsMeasuresData(queryGrouped.current.getData('metrics_grouped'));
                 setDataLoadingStatus(false);
 
             });
         } else {
-            console.log('cleared measure data')
-            setLocationsMeasuresData([]);
+
             setDataLoadingStatus(false);
         }
 
     }, [queryLocationsFilter, queryPeriodFilter, props.typeFilter])
 
-    const updateLocationsFilter = (value: any) => {
-        if (value) {
-            setLocationsFilter(value.toString().split(','));
-        } else {
-            setLocationsFilter([]);
-        }
 
-    }
 
     const applyPeriodFilter = (value: any) => {
         setQueryPeriodFilter(value);
         setPeriodFilter(value);
     }
 
-    const applyLocationFilter = () => {
-        setQueryLocationsFilter(locationsFilter);
-    }
 
 
     const stringArrayToString = (value: string[]) => {
@@ -198,144 +168,12 @@ export default function AllMetrics(props: AllMetricsProps) {
             title: 'Commentary',
             dataIndex: 'commentary',
         },
-        // {
-        //     title: 'Cases',
-        //     dataIndex: 'cases',
-        //     className: 'column-number',
-        //     // @ts-ignore
-        //     sorter: (a, b) => a.cases - b.cases
-        // },
-        // {
-        //     title: 'Deaths',
-        //     dataIndex: 'deaths',
-        //     className: 'column-number',
-        //     // @ts-ignore
-        //     sorter: (a, b) => a.deaths - b.deaths
-        //
-        // },
-        //
-        // {
-        //     title: 'New Cases',
-        //     dataIndex: 'newcases',
-        //     className: 'column-number',
-        //     // @ts-ignore
-        //     sorter: (a, b) => a.newcases - b.newcases
-        //
-        // },
-        // {
-        //     title: 'New Deaths',
-        //     dataIndex: 'newdeaths',
-        //     className: 'column-number',
-        //     // @ts-ignore
-        //     sorter: (a, b) => a.newdeaths - b.newdeaths
-        // },
-        // {
-        //     title: 'Recovered',
-        //     dataIndex: 'recovered',
-        //     className: 'column-number',
-        //     // @ts-ignore
-        //     sorter: (a, b) => a.recovered - b.recovered
-        // },
-        // {
-        //     title: 'cR',
-        //     dataIndex: 'cr',
-        //     className: 'column-number',
-        //     // @ts-ignore
-        //     sorter: (a, b) => a.cr - b.cr
-        // },
-        // {
-        //     title: 'mR',
-        //     dataIndex: 'mr',
-        //     className: 'column-number',
-        //     // @ts-ignore
-        //     sorter: (a, b) => a.mr - b.mr
-        //
-        // },
-        // {
-        //     title: 'R',
-        //     dataIndex: 'r',
-        //     className: 'column-number',
-        //     // @ts-ignore
-        //     sorter: (a, b) => a.r - b.r
-        // },
-        // {
-        //     title: 'sdIndicator',
-        //     dataIndex: 'sdindicator',
-        //     className: 'column-number',
-        //     // @ts-ignore
-        //     sorter: (a, b) => a.sdindicator - b.sdindicator
-        // },
-        // {
-        //     title: 'medIndicator',
-        //     dataIndex: 'medindicator',
-        //     className: 'column-number',
-        //     // @ts-ignore
-        //     sorter: (a, b) => a.dmr - b.dmr
-        // },
-        // {
-        //     title: 'iMort',
-        //     dataIndex: 'imort',
-        //     className: 'column-number',
-        //     // @ts-ignore
-        //     sorter: (a, b) => a.imort - b.imort
-        //
-        // },
+
     ];
 
-    const rowSelection = {
-        onChange: (selectedRowKeys: any, selectedRows: any) => {
-            updateLocationsFilter(selectedRowKeys);
-        },
-        selectedRowKeys: locationsFilter,
-
-    };
 
 
-    const definitions = (
-        <div style={{width: 500}}>
-            <Descriptions column={1}>
-                <Descriptions.Item label="cR">
-                    Compound growth factor for Confirmed Cases. This is an indicator of the degree of spread and
-                    should decrease with social distancing. In the absence of immunity and social mitigation's,
-                    this would be equal to R0 (the natural spread rate of a virus).
-                </Descriptions.Item>
-                <Descriptions.Item label="mR">
-                    Compound growth rate for Deaths. Though this lags the cR, it may be a better proxy for
-                    actual number of cases because it is not confounded by test availability and policy.
-                </Descriptions.Item>
-                <Descriptions.Item label="sdIndicator">
-                    Shows progress in social distancing behavior from period to period. Positive values indicate
-                    progress in slowing the spread. Negative values indicate that the rate of spread is
-                    increasing.
 
-                </Descriptions.Item>
-                <Descriptions.Item label="medIndicator">
-                    Shows progress in medical performance from period to period. Positive values indicate
-                    improvement. Negative values may indicate overload conditions. A negative value early on in
-                    a
-                    given location’s progress may indicate a lack of testing or diagnosis. A return to negative
-                    later in the cycle can indicate an erosion in the quality of medical care.
-                </Descriptions.Item>
-
-                <Descriptions.Item label="iMort">
-                    This is an approximation of Infection Mortality which is the likelihood that someone who
-                    tests positive for the infection will die as a result of the infection. This number may be
-                    exaggerated during the very early stages of the infection in a location due to lack of
-                    diagnosis and testing.
-                </Descriptions.Item>
-                <Descriptions.Item label="HeatIndex">
-                    This is a composite indicator designed to reflect a broad aspect of a location’s progress.
-                    Higher values indicate locations with higher risk. This indicator combines Case Growth (cR),
-                    Mortality Growth (mR), Social Distancing (sdIndicator) and Medical Indicator (medIndicator).
-                    This indicator is designed to flag locations that are in need of attention, due to high
-                    rates of
-                    spread or that have begun to regress (e.g. as a result of loosened social distancing or
-                    medical
-                    overload).
-                </Descriptions.Item>
-            </Descriptions>
-        </div>
-    );
 
     return (
 
@@ -352,36 +190,6 @@ export default function AllMetrics(props: AllMetricsProps) {
 
             <LocationDetails show={showLocationDetails}/>
 
-
-
-            {/*<Tabs defaultActiveKey="1" onChange={(key) => {*/}
-            {/*    applyLocationFilter()*/}
-            {/*}}>*/}
-
-            {/*    /!*<div style={{height:20}}/>*!/*/}
-            {/*    <TabPane tab="Analysis" key="1">*/}
-
-
-            {/*        <div style={{height: 20}}/>*/}
-
-            {/*        <Popover placement="rightTop" title={'Metrics Definitions'} content={definitions} trigger="click"*/}
-            {/*                 style={{width: 300}}>*/}
-            {/*            <Button>Metrics Definitions</Button>*/}
-            {/*        </Popover>*/}
-
-            {/*        <div style={{height: 20}}/>*/}
-
-
-            {/*        <ChartX data={locationsMeasuresData} groupFiled={'measure'} labelField={'locationstatus'}*/}
-            {/*                valueFiled={'value'} height={filterLength * 200}/>*/}
-            {/*    </TabPane>*/}
-
-
-            {/*    <TabPane tab="Data & Locations Filter" key="2">*/}
-
-            {/*        <div style={{height: 20}}/>*/}
-
-
             <FilterRenderer title={'Select a Period'} data={periodsCatalog} value={periodFilter}
                             mode={undefined}
                             onFilterChange={(value) => applyPeriodFilter(value)}/>
@@ -393,9 +201,6 @@ export default function AllMetrics(props: AllMetricsProps) {
                     <Table size={'small'} rowKey={'location'} bordered columns={layout} dataSource={allMeasuresData}
                            pagination={{pageSize: 50}} loading={{spinning: dataLoadingStatus}}
                            />
-
-            {/*    </TabPane>*/}
-            {/*</Tabs>*/}
 
 
         </Layout>
